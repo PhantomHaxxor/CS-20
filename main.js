7d2b2677d8429455d9eef1ff89ae7e681c6e0dc6
PhantomHaxxor <73779501+PhantomHaxxor@users.noreply.github.com>
2021-05-05 14:37:59 -0600

    investment assignment

--- main.js
+++ main.js
@@ -11,15 +11,25 @@ let accounts = [];
 let maxAmount = 5000; // account values should be b/t 0 and max
 
 // Display Data
-drawArray();
+drawArray(true);
 
-function drawArray() {
+function drawArray(FirstSetup) {
   let outputStr = "";
   let divHeight;
-  for (let i = 0; i < accounts.length; i++) {
-    divHeight = (accounts[i] / maxAmount) * 600; // Scale accounts to fit in array visualizer container
-    outputStr += `<div style="height:${divHeight}px"></div>`;
+  if (FirstSetup === true) {
+    for (let i = 0; i <= 200; i++) {
+      var randomNum = Math.floor(RandomInt(0, 5000))
+      divHeight = (randomNum / maxAmount) * 600;
+      outputStr += `<div style="height:${divHeight}px"></div>`;
+      accounts[i] = randomNum
+    }
+  } else {
+    for (let i = 0; i < accounts.length; i++) {
+      divHeight = (accounts[i] / maxAmount) * 600; // Scale accounts to fit in array visualizer container
+      outputStr += `<div style="height:${divHeight}px"></div>`;
+    }
   }
+
   containerEl.innerHTML = outputStr;
 }
 
@@ -48,7 +58,7 @@ function mainMenu() {
   }
 
   // Redraw array to show any changes
-  drawArray();
+  drawArray(false);
 }
 
 // ******************************************************
@@ -56,8 +66,13 @@ function mainMenu() {
 // ******************************************************
 function countRange() {
   // Output the number of accounts with amounts between $2,000 and $4,000, inclusive
-
-  outputEl.innerHTML = "Count Range";
+  var count = 0
+  for (let i = 0; i < accounts.length; i++) {
+    if (accounts[i] >= 2000 && accounts[i] <= 4000) {
+      count++
+    }
+  }
+  outputEl.innerHTML = "Accounts between $2000 and $4000: " + count;
 }
 
 function generousDonor() {
@@ -65,38 +80,75 @@ function generousDonor() {
   // account that has less than $2000. 
   // Modify the investment account array to apply this donation.
   // Output the total amount of money that was donated.
-
-  outputEl.innerHTML = "Generous Donor";
+  var TotalDonated = 0
+  for (let i = 0; i < accounts.length; i++) {
+    if (accounts[i] < 2000) {
+      TotalDonated += 500
+      accounts[i] += 500
+    }
+  }
+  outputEl.innerHTML = "Generous donator donated a total of $" + TotalDonated;
 }
 
 function hackerAttack() {
   // A hacker steals 5% from every account.
   // Modify the investment account array to apply this theft.
   // Output the total amount that was stolen.
-
-  outputEl.innerHTML = "Hacker Attack";
+  var TotalStolen = 0
+  for (let i = 0; i < accounts.length; i++) {
+    var FivePercent = Math.floor((5 / 100) * accounts[i])
+    TotalStolen += FivePercent
+    accounts[i] -= FivePercent
+  }
+  outputEl.innerHTML = "Hacker Attack | A Hacker stole a total of $" + TotalStolen;
 }
 
 function investmentStats() {
   // Output the minimum account amount, the maximum account amount
   // and the average account amount.
+  var MinimumAmount
+  var MaximumAmount
+  var Total = 0
+  for (let i = 0; i < accounts.length; i++) {
+    if (MinimumAmount == null) {
+      MinimumAmount = accounts[i]
+    }
+    if (MaximumAmount == null) {
+      MaximumAmount = accounts[i]
+    }
 
-  outputEl.innerHTML = "Investment Stats";
+    if (accounts[i] < MinimumAmount) {
+      MinimumAmount = accounts[i]
+    }
+    if (accounts[i] > MaximumAmount) {
+      MaximumAmount = accounts[i]
+    }
+    Total += accounts[i]
+  }
+  var Average =  Math.floor(Total / accounts.length)
+  outputEl.innerHTML = "Investment Stats | Minimum: $" + MinimumAmount + " The Maximum: $" + MaximumAmount + " The Average: $" + Average;
 }
 
 function addAccount() {
   // Prompt for a new account amount and add this to the invesment account
   // array. Output a confirmation that a new account was added with an
   // opening amount of _______.
-
-  outputEl.innerHTML = "Add Account";
+  var randomNum = Math.floor(RandomInt(0, 5000))
+  accounts.push(randomNum)
+  outputEl.innerHTML = "Added account with amount: $" + randomNum;
 }
 
 function removeLow() {
   // Remove all accounts that are below $500.
   // Output how many accounts were removed.
-
-  outputEl.innerHTML = "Remove Low Accounts";
+  var AccountsRemoved = 0
+  for (let i = 0; i < accounts.length; i++) {
+    if (accounts[i] < 500) {
+      accounts.splice(i, 1)
+      AccountsRemoved++
+    }
+  }
+  outputEl.innerHTML = "Removed " + AccountsRemoved + " Low Account(s)";
 }
 
 function robinHood() {
@@ -106,6 +158,26 @@ function robinHood() {
   // accounts that have less than $1000.
   // Output how many accounts received money and 
   // how much each account received.
+  var TotalAmountTaken = 0
+  var AccountsUnder1000 = 0
+  for (let i = 0; i < accounts.length; i++) {
+    if (accounts[i] > 4000) {
+      TotalAmountTaken += 400
+      accounts[i] -= 400
+    }
 
-  outputEl.innerHTML = "Robin Hood";
+    if (accounts[i] < 1000) {
+      AccountsUnder1000++
+    }
+  }
+
+  var AverageAmountToGive = Math.floor(TotalAmountTaken / AccountsUnder1000)
+
+  for (let i = 0; i < accounts.length; i++) {
+    if (accounts[i] < 1000) {
+      accounts[i] += AverageAmountToGive
+    }
+  }
+
+  outputEl.innerHTML = "Robin Hood | " + AccountsUnder1000 + " accounts recieved money | each account recieved: $" + AverageAmountToGive;
 }
---
+++ randomLib.js
@@ -0,0 +1,5 @@
+// Random Library
+
+function RandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
