0a26a3146f1b0e848e1d1dee3eb17601c91900b9
Daniel Mughal <73779501+PhantomHaxxor@users.noreply.github.com>
2021-06-17 15:00:30 -0600

    Finish

--- JS/Homepage.js
+++ JS/Homepage.js
@@ -2,11 +2,52 @@ var LogoutBtn = document.getElementsByClassName("LogoutBtn")[0]
 var toggleButton = document.getElementsByClassName('toggle-button')[0]
 var navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
+var VolumeAmount = document.getElementById("VolumeAmount")
+
+var image = document.querySelector('#cover');
+var title = document.getElementById('title');
+var artist = document.getElementById('artist');
+var music = document.getElementById('SongAudio');
+var currentTimeEl = document.getElementById('current-time');
+var durationEl = document.getElementById('duration');
+var progress = document.getElementById('progress');
+var progressContainer = document.getElementById('progress-container');
+var prevBtn = document.getElementById('prev');
+var playBtn = document.getElementById('play');
+var nextBtn = document.getElementById('next');
+
+var e = document.querySelector('.volume-slider-div');
+var eInner = document.querySelector('.volume-slider');
+
+var isDragging = false;
+let isPlaying = false;
+
 var Settings = {
     LoginData: JSON.parse(localStorage.getItem("LoginData")),
     Debounce: false,
     DelayTime: 1000,
 }
+
+var songs = [
+  {
+    path: '/Music/Arcade.mp3',
+    displayName: 'Arcade',
+    artist: 'Duncan Laurence',
+    cover: "https://i.ytimg.com/vi/PNozaFzqOvI/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBBm8vXh7QF4OYlKWf9kkSoW6NLXA",
+  },
+  {
+    path: '/Music/FlyMeToTheMoon.mp3',
+    displayName: 'Fly Me To The Moon',
+    artist: 'Frank Sinatra',
+    cover: "https://i.ytimg.com/vi/ZEcqHA7dbwM/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBHE91OZhtBXktw7JhT1-zIN9HyGA",
+  },
+  {
+    path: '/Music/AsTheWorldCavesIn.mp3',
+    displayName: 'As the World Caves In',
+    artist: 'Matt Maltese',
+    cover: "https://i.ytimg.com/vi/yS2KyK3pqj4/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDSvZXFHzHdiEzYJUv_A4K0arp3cA",
+  },
+];
 
 // Small Functions //
 function ToggleNavBar() {
@@ -22,7 +63,6 @@ function RemoveSignedIn() {
     localStorage.setItem("LoginData", StringifiedArray)
 }
 
-
 // Big Functions //
 function OnWindowLoad() {
     // Runs when window loads and logs out a user if they wern't signed in //
@@ -31,77 +71,45 @@ function OnWindowLoad() {
     console.log(Settings.LoginData)
 
     // Default LoginData Template //
-    if (Settings.LoginData == null || Settings.LoginData.AccountCreated == false) {
+    if (Settings.LoginData.AccountCreated == false || Settings.LoginData.SignedIn == false) {
         Logout()
         return
     }
 }
 
 function Logout() {
-    console.log("Log out")
-
     // Make user logout by getting rid of their current data //
-    if (Settings.LoginData && Settings.LoginData.AccountCreated == true) {
+    if (Settings.LoginData.AccountCreated == true) {
         RemoveSignedIn()
     }
 
     window.location.href = window.location.origin
 }
 
-// Event Listeners //
-OnWindowLoad()
-toggleButton.addEventListener('click', ToggleNavBar)
+var updateBar = function (x, vol) {
+   var volume = e;
+        var percentage;
+        //if only volume have specificed
+        //then direct update volume
+        if (vol) {
+            percentage = vol * 100;
+        } else {
+            var position = x - volume.offsetLeft;
+            percentage = 100 * position / volume.clientWidth;
+        }
 
+        if (percentage > 100) {
+            percentage = 100;
+        }
+        if (percentage < 0) {
+            percentage = 0;
+        }
 
-
-const image = document.querySelector('#cover');
-const title = document.getElementById('title');
-const artist = document.getElementById('artist');
-const music = document.querySelector('audio');
-const currentTimeEl = document.getElementById('current-time');
-const durationEl = document.getElementById('duration');
-const progress = document.getElementById('progress');
-const progressContainer = document.getElementById('progress-container');
-const prevBtn = document.getElementById('prev');
-const playBtn = document.getElementById('play');
-const nextBtn = document.getElementById('next');
-
-// Music
-const songs = [
-  {
-    path: 'https://raw.githubusercontent.com/ustabasiibrahim/music-player/master/assets/music/1.mp3',
-    displayName: 'Yıldız Tozu',
-    artist: 'Ozbi',
-    cover: "https://images.genius.com/ee202c6f724ffd4cf61bd01a205eeb47.1000x1000x1.jpg",
-  },
-  {
-    path: 'https://raw.githubusercontent.com/ustabasiibrahim/music-player/master/assets/music/2.mp3',
-    displayName: 'You\'re Somebody Else',
-    artist: 'flora cash',
-    cover: "https://pbs.twimg.com/media/D2NZH-2U4AAL9Xs.jpg",
-  },
-  {
-    path: 'https://raw.githubusercontent.com/ustabasiibrahim/music-player/master/assets/music/3.mp3',
-    displayName: 'Powerless',
-    artist: 'Linkin Park',
-    cover: "https://images.genius.com/c5a58cdaab9f3199214f0e3c26abbd0e.1000x1000x1.jpg",
-  },
-  {
-    path: 'https://raw.githubusercontent.com/ustabasiibrahim/music-player/master/assets/music/4.mp3',
-    displayName: 'Seni Dert Etmeler',
-    artist: 'Madrigal',
-    cover: "https://www.radyomega.fm/wp-content/uploads/2020/04/MADRIGAL-600.jpg",
-  },
-  {
-    path: 'https://raw.githubusercontent.com/ustabasiibrahim/music-player/master/assets/music/5.mp3',
-    displayName: 'Ederlezi',
-    artist: 'Solomun',
-    cover: "https://m.media-amazon.com/images/I/616t0841uvL._SS500_.jpg",
-  },
-];
-
-// Check if Playing
-let isPlaying = false;
+        //update volume bar and video volume
+        eInner.style.width = percentage +'%';
+        VolumeAmount.innerText = "Volume: " + Math.floor(percentage) +'%'
+        music.volume = percentage / 100;
+};
 
 // Play
 function playSong() {
@@ -119,10 +127,6 @@ function pauseSong() {
   music.pause();
 }
 
-// Play or Pause Event Listener
-playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
-
-// Update DOM
 function loadSong(song) {
   title.textContent = song.displayName;
   artist.textContent = song.artist;
@@ -132,10 +136,8 @@ function loadSong(song) {
 
 function changeCover(cover) {
   image.classList.remove('active');
-  setTimeout(() => {
-    image.src = cover;
-    image.classList.add('active');
-  }, 100)
+  image.src = cover;
+  image.classList.add('active')
 } 
 
 // Current Song
@@ -200,8 +202,26 @@ function setProgressBar(e) {
 }
 
 // Event Listeners
+OnWindowLoad()
+
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
 music.addEventListener('ended', nextSong);
 music.addEventListener('timeupdate', updateProgressBar);
 progressContainer.addEventListener('click', setProgressBar);
+toggleButton.addEventListener('click', ToggleNavBar)
+
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
+e.addEventListener('mousedown',function(ev){
+  isDragging = true;
+  updateBar(ev.clientX);
+});
+document.addEventListener('mousemove',function(ev){
+  if(isDragging){
+     updateBar(ev.clientX);
+  }
+});
+document.addEventListener('mouseup',function(ev){
+isDragging = false;
+});
