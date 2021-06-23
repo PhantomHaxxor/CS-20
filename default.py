e3cc28bccc5ec14a73bf92b323f3f9577bc2476d
Daniel Mughal <73779501+PhantomHaxxor@users.noreply.github.com>
2021-06-23 12:25:52 -0600

    Update default.py

--- default.py
+++ default.py
@@ -1,12 +1,55 @@
-
-
 import math
 
-A = int(input("What is A? "))
-B = int(input("What is B? ")) 
+# Quiz
 
-C_Squared = math.pow(A, 2) + math.pow(B, 2)
-C_Side = math.sqrt(C_Squared)
+CurrentScore = 0
 
-Length = round(C_Side, 2)
-print("C Squareed is " + str(C_Squared) + ". The Length of the C Side is " + str(Length))
+# Question 1
+
+Q1_Answer = input("What is 2 + 2? ")
+
+if int(Q1_Answer) == 4:
+    print("Correct!")
+    CurrentScore += 1
+else:
+    print("Incorrect")
+
+# Question 2
+
+Q2_Answer = input("What is 5^2? ")
+
+if int(Q2_Answer) == math.pow(5, 2):
+    print("Correct!")
+    CurrentScore += 1
+else:
+    print("Incorrect")
+
+# Question 3
+
+Q3_Answer = input("What is the smallest planet in our solar system? ")
+
+if Q3_Answer == "Mercury":
+    print("Correct!")
+    CurrentScore += 1
+else:
+    print("Incorrect")
+
+Q4_Answer = input("What is the scientific name for water? ")
+
+if str.lower(Q4_Answer) == "h2o":
+    print("Correct!")
+    CurrentScore += 1
+else:
+    print("Incorrect")
+
+print(CurrentScore)
+
+if CurrentScore == 1:
+    print("You got 1/4 | You need to study more")
+elif CurrentScore == 2:
+    print("You got 2/4 | You're halfway there!")
+elif CurrentScore == 3:
+    print("You got 3/4, Nice!")
+elif CurrentScore == 4:
+      print("You got 4/4, Congratulations!")  
+
