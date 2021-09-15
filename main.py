65a6a224d41ab6a7b509078b2677dc49a6082f3b
PhantomHaxxor <73779501+PhantomHaxxor@users.noreply.github.com>
2021-09-15 13:27:14 -0600

    added more stuff

--- main.py
+++ main.py
@@ -1,32 +1,98 @@
 import requests
 
-username = input("What is your ROBLOX Name")
-req = requests.post(
-    url = "https://users.roblox.com/v1/usernames/users",
-    data =  {
-        "usernames": [
-            username
-        ],
-        "excludeBannedUsers": False
-    }
-)
-userData = req.json()
-user_id = userData["data"]
-print(user_id)
 
-user_req = requests.get(f"https://users.roblox.com/v1/users/{user_id}")
-user_data = user_req.json()
+Enabled = True
 
-print("Name:", user_data["name"])
-print("Display Name:", user_data["displayName"])
-print("User ID:", user_data["id"])
-print("Description:")
-print(user_data["description"])
+def GetOutfitsFromUserId(user_id):
+    request = requests.get(f"https://avatar.roblox.com/v1/users/{user_id}/outfits")
+    response = dict()
 
-user_friend_req = requests.get(f"https://friends.roblox.com/v1/users/{user_id}/friends")
-user_friend_data = user_friend_req.json()
+    if request.status_code == 200:
+        response["status"] = True
+        response["data"] = request.json()["data"]
+    else:
+        response["status"] = False
+    return response
 
-friend_data = user_friend_data["data"]
+def GetFriendsFromUserId(user_id):
+    request = requests.get(f"https://friends.roblox.com/v1/users/{user_id}/friends")
+    response = dict()
 
-for i in friend_data:
-    print(i["name"])
+    if request.status_code == 200:
+        response["status"] = True
+        response["data"] = request.json()["data"]
+    else:
+        response["status"] = False
+
+    return response
+
+def GetUserIdFromUsername(username):
+    request = requests.post(
+        url = "https://users.roblox.com/v1/usernames/users",
+        data =  {
+            "usernames": [
+                username
+            ],
+            "excludeBannedUsers": True
+        }
+    )
+    response = dict()
+    response["status"] = False
+
+    if request.status_code == 200:
+        data_table = request.json()["data"]
+
+        if data_table == None:
+            response["status"] = False
+        if data_table == None:
+            response["status"] = False
+        print(data_table[0])
+
+        if data_table[0]: 
+            response["status"] = True
+            response["data"] = data_table[0]['id']
+    else:
+        response["status"] = False
+
+    return response
+
+while Enabled:
+    username = input("What is your ROBLOX Username?: \n")
+    
+    UserIdResponse = GetUserIdFromUsername(username)
+    if UserIdResponse["status"] == False:
+        print("Error Getting Data from Username, please retry \n")
+        continue
+    user_id = UserIdResponse["data"]
+
+    FriendsResponse = GetFriendsFromUserId(user_id)
+    if FriendsResponse["status"] == False:
+        print("Error Getting Friends Data, please retry \n")
+        continue
+    friends_data = FriendsResponse["data"]
+    
+    print("Your UserId: {user_id}".format(user_id = user_id))
+
+    if len(friends_data) == 0:
+        print(username + " does not have any friends")
+    else:
+        print("Here is your ROBLOX friends list: \n")
+        for i in friends_data:
+            print("Username: {username}, UserId: {userid}".format(username = i["name"], userid = i["id"]))
+
+    answer = input("Do you want your outfit details (y/n): \n")
+    if answer.lower() == "n":
+        continue
+
+    outfitsResponse = GetOutfitsFromUserId(user_id)
+    if outfitsResponse["status"] == False:
+        print("Error Getting Outfits Data, please retry \n")
+        continue
+    outfit_data = outfitsResponse["data"]
+    
+    print("\nHere is your Outfit Data\n")
+    for i in outfit_data:
+        print("Outfit Name: {outfit_name}".format(outfit_name = i["name"]))
+    print("\n")
+
+    
