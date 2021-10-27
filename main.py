666cf59834261871acf61bb86b926428fd3c0c4c
PhantomHaxxor <73779501+PhantomHaxxor@users.noreply.github.com>
2021-10-27 12:49:47 -0600

    a

--- main.py
+++ main.py
@@ -1,31 +1,4 @@
-import sys
-sys.path.append("widgets")
-import loginWidget
-import pageWidget
 
-currentWidget = None
-
-shouldAutoLogin = False
-loginUsername = "Test123"
-loginPassword = "Test123"
-
-def autoLogin():
-   if currentWidget.doesAccountExist(loginUsername):
-        currentWidget.attemptLogin(loginUsername, loginPassword)
-
-def loginSuccess(accountData):
-    global currentWidget
-    currentWidget.destroyWindow()
-    currentWidget = pageWidget.new(accountData)
-
-def createLoginWidget():
-    global currentWidget
-    currentWidget = loginWidget.new(loginSuccess)
-    
-    if shouldAutoLogin:
-        autoLogin()
-
-    currentWidget.mainloop()
 
 if __name__ == "__main__":
-    createLoginWidget()
+    print("a")
