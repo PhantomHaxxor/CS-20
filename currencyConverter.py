beed6630a8238290bf8fcd067ae2a84fa040ff48
PhantomHaxxor <73779501+PhantomHaxxor@users.noreply.github.com>
2021-11-03 12:28:33 -0600

    fixes

--- currencyConverter.py
+++ currencyConverter.py
@@ -1,5 +1,3 @@
-# # Python Project on Currency Converter
-
 import requests
 from tkinter import *
 import tkinter as tk
@@ -13,7 +11,7 @@ class RealTimeCurrencyConverter():
 
     def convert(self, from_currency, to_currency, amount): 
         if from_currency != 'USD' : 
-            amount = amount / self.currencies[from_currency] 
+            amount /= self.currencies[from_currency] 
   
         # limiting the precision to 4 decimal places 
         amount = round(amount * self.currencies[to_currency], 4) 
@@ -28,26 +26,22 @@ class App(tk.Tk):
 
         #self.configure(background = 'blue')
         self.geometry("500x200")
+        self.resizable(0,0)
         
         # Label
-        self.intro_label = Label(self, text = 'Welcome to Real Time Currency Convertor',  fg = 'blue', relief = tk.RAISED, borderwidth = 3)
-        self.intro_label.config(font = ('Courier',15,'bold'))
-
-        self.date_label = Label(self, text = f"1 Indian Rupee equals = {self.currency_converter.convert('INR','USD',1)} USD \n Date : {self.currency_converter.data['date']}", relief = tk.GROOVE, borderwidth = 5)
-
-        self.intro_label.place(x = 10 , y = 5)
-        self.date_label.place(x = 160, y= 50)
+        self.date_label = Label(self, text = f"1 USD = { self.currency_converter.convert('USD','CAD',1) } CAD", relief = tk.GROOVE, borderwidth = 5)
+        self.date_label.place(x = 160, y= 10)
 
         # Entry box
-        valid = (self.register(self.restrictNumberOnly), '%d', '%P')
+        valid = (self.register(self.restrictNumberOnly), '%P')
         self.amount_field = Entry(self,bd = 3, relief = tk.RIDGE, justify = tk.CENTER,validate='key', validatecommand=valid)
         self.converted_amount_field_label = Label(self, text = '', fg = 'black', bg = 'white', relief = tk.RIDGE, justify = tk.CENTER, width = 17, borderwidth = 3)
 
         # dropdown
         self.from_currency_variable = StringVar(self)
-        self.from_currency_variable.set("INR") # default value
+        self.from_currency_variable.set("USD") # default value
         self.to_currency_variable = StringVar(self)
-        self.to_currency_variable.set("USD") # default value
+        self.to_currency_variable.set("CAD") # default value
 
         font = ("Courier", 12, "bold")
         self.option_add('*TCombobox*Listbox.font', font)
@@ -55,16 +49,16 @@ class App(tk.Tk):
         self.to_currency_dropdown = ttk.Combobox(self, textvariable=self.to_currency_variable,values=list(self.currency_converter.currencies.keys()), font = font, state = 'readonly', width = 12, justify = tk.CENTER)
 
         # placing
-        self.from_currency_dropdown.place(x = 30, y= 120)
-        self.amount_field.place(x = 36, y = 150)
-        self.to_currency_dropdown.place(x = 340, y= 120)
+        self.from_currency_dropdown.place(x = 30, y= 75)
+        self.amount_field.place(x = 36, y = 100)
+        self.to_currency_dropdown.place(x = 340, y= 75)
         #self.converted_amount_field.place(x = 346, y = 150)
-        self.converted_amount_field_label.place(x = 346, y = 150)
+        self.converted_amount_field_label.place(x = 346, y = 100)
         
         # Convert button
         self.convert_button = Button(self, text = "Convert", fg = "black", command = self.perform) 
         self.convert_button.config(font=('Courier', 10, 'bold'))
-        self.convert_button.place(x = 225, y = 135)
+        self.convert_button.place(x = 225, y = 150)
 
     def perform(self):
         amount = float(self.amount_field.get())
@@ -76,7 +70,7 @@ class App(tk.Tk):
 
         self.converted_amount_field_label.config(text = str(converted_amount))
     
-    def restrictNumberOnly(self, action, string):
+    def restrictNumberOnly(self, string):
         regex = re.compile(r"[0-9,]*?(\.)?[0-9,]*$")
         result = regex.match(string)
         return (string == "" or (string.count('.') <= 1 and result is not None))
